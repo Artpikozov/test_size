d325fef8258ad0c10d04e08ea4296c3c8c6a703d
Artpikozov <pikozovartem@mail.ru>
2025-06-09 10:09:20 +0300

    domashka

--- test_size.py
+++ test_size.py
@@ -1,10 +1,14 @@
 from selene import browser, be, have
 
+
 def test(setting_browser):
     browser.open('https://duckduckgo.com/')
     browser.element('[name="q"]').should(be.blank).type('тестирование ПО').press_enter()
     browser.element('html').should(have.text('Теория тестирования ПО просто и понятно / Хабр'))
+
+
 def test_1(setting_browser):
     browser.open('https://duckduckgo.com/')
-    browser.element('[name="q"]').should(be.blank).type('ea23451213grhgshv234shvrtvh43532nhstnns3245tr').press_enter()
-    browser.element('html').should(have.text('привет'))
+    browser.element('[name="q"]').should(be.blank).type(
+        '21123453426532464541234231424354343641215312435346511').press_enter()
+    browser.element('html').should(have.text('ничего не найдено.'))
